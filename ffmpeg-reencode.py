7a022185d416b44e78e5e76bc886898a70061c96
Damien Frémont <damien.fremont@gmail.com>
2022-09-13 23:38:53 +0200

    feat: encode skip h265

--- ffmpeg-reencode.py
+++ ffmpeg-reencode.py
@@ -1,40 +1,47 @@
-import sys, os, glob, getopt, time, pathlib
+import sys, os, glob, getopt, subprocess, pathlib, json
+from subprocess import call
+from os.path import exists
 
 # STATIC **********************************************************************
 
 DEFAULT_EXTS = ['mp4']
 DEFAULT_CODECS = ['h264']
-TMP_DIR = '_tmp'
-# RES_PATH = os.path.join(os.path.dirname(os.path.realpath(__file__)), RES_DIR)
 
 # PUBLIC **********************************************************************
 
 class Item:
     index = 0
     path = ""
-    desfile = ""
+    dest = ""
     thumb = ""
     
 # PRIVATE *********************************************************************
 
 def process(videosdirpath):
+    print(f"process()...")
     items = []
     print("STEP 1 : FIND")
     items = listfiles(videosdirpath)
     items = filterbyext(items, DEFAULT_EXTS)
-    # items = filterbycodec(items, DEFAULT_CODECS)   
+    items = filterbycodec(items, DEFAULT_CODECS)   
     print("STEP 2 : PREPARE")
     # items = createthumbs(items)
     # items = namedateprefix(items)
     print("STEP 3 : ENCODE")
-    # items = namecodecsuffix(items)
-    # items = reencode(items)
+    for item in items:
+        print(f"{item.index}: {item.path}")
+        item = setdestination(item)
+        item = replacesuffix(item, '-x265.mp4')
+        if exists(item.dest) :
+            break
+        item = reencode(item)
     print("STEP 4 : ARCHIVE")
     # items = createarchivedir()
     # items = archive(items)
     print('Batch Result:')
 
 def listfiles(path):
+    print(f"listfiles() path={path}...")
     items = []
     files = glob.glob(f"{path}/*.*")
     index = 0
@@ -47,7 +54,7 @@ def listfiles(path):
     return items
 
 def filterbyext(items, exts):
-    print(f"finding files with exts={exts}...")
+    print(f"filterbyext() exts={exts}...")
     filtered = []
     for item in items:
         ext = pathlib.Path(item.path).suffix
@@ -56,10 +63,61 @@ def filterbyext(items, exts):
         if ext in exts:
             filtered.append(item)
             print(f"{item.index}: {item.path}")
-    return items
+    return filtered
 
-def getfileext(path):
-    return 
+def filterbycodec(items, codecs):
+    print(f"filterbycodec() exts={codecs}...")
+    filtered = []
+    for item in items:
+        metadata = FFProbe(item.path)
+        for stream in metadata["streams"]:
+            if is_video(stream):
+                if codec(stream).lower() in codecs:
+                    filtered.append(item)
+                    print(f"{item.index}: {item.path}")
+    return filtered
+
+# https://github.com/gbstack/ffprobe-python
+def FFProbe(path):
+    cmd = f"ffprobe.exe -show_format -show_streams -loglevel quiet -print_format json {path}"
+    # print(cmd)
+    json_data = subprocess.check_output(cmd, shell=True)
+    json_object = json.loads(json_data)
+    # json_formatted_str = json.dumps(json_object, indent=2)
+    # print(json_formatted_str)
+    metadata = json_object
+    return metadata
+
+# https://github.com/gbstack/ffprobe-python
+def is_video(json):
+    return json['codec_type'] == 'video'
+# https://github.com/gbstack/ffprobe-python
+def codec(json):
+    return json['codec_name']
+
+def setdestination(item):
+    print(f"setdestination()...")
+    item.dest = item.path
+    print(f"{item.index}: {item.dest}")
+    return item
+
+def replacesuffix(item, suffix):
+    print(f"replacesuffix() suffix={suffix}...")
+    path = item.dest
+    ext = pathlib.Path(path).suffix
+    new = path.replace(ext, f"{suffix}")
+    item.dest = new
+    print(f"{item.index}: {item.dest}")
+    return item
+
+def reencode(item):
+    print(f"reencode()...")
+    input = item.path
+    output = item.dest
+    cmd = f"ffmpeg.exe -i \"{input}\" -c:v libx265 \"{output}\""
+    print(cmd)
+    out = subprocess.check_output(cmd, shell=True)
+    return item
 
 # https://www.tutorialspoint.com/python/python_command_line_arguments.htm
 def getargs(argv, configs, helpmsg=None):
