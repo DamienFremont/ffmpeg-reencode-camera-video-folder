aa1c6eb8ec6f5b4ff48357cfb649f78f88799489
Damien Frémont <damien.fremont@gmail.com>
2022-11-01 23:09:47 +0100

    warn: disable clean task

--- run.bat
+++ run.bat
@@ -3,4 +3,4 @@ SET DIRPATH="C:\Users\damien\Desktop\100MEDIA"
 
 python .\ffmpeg-renamectime.py --dirpath %DIRPATH%
 python .\ffmpeg-reencodex265.py --dirpath %DIRPATH%
-python .\ffmpeg-clean.py --dirpath %DIRPATH%
+@REM python .\ffmpeg-clean.py --dirpath %DIRPATH%
